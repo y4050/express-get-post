a76786d4365a9111ce0e598bdf2a79a0ed3a8296
J <j@Jacobs-MacBook-Pro.local>
2021-02-02 12:37:58 -0800

    updated

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const fs = require("fs");
+
+// MiddleWare
+// this will help us use our layout file
+app.use(expressLayouts);
+app.use(express.urlencoded({extended: false}));
+
+// for views use .ejs files
+app.set("view engine", "ejs");
+
+// ROUTE
+app.get("/", (req, res) => {
+    res.send("Hi there");
+});
+
+// Index View
+// this url: localhost:8000/dionsaurs
+app.get("/dinosaurs", (req, res) => {
+    // in our views folder render this page
+    let dinos = fs.readFileSync("./dinosaurs.json");
+    // take out data and put it in a more readable format
+    dinos = JSON.parse(dinos);
+    console.log(dinos);
+    // in our views folder render this page
+    res.render("dinosaurs/index", { dinos: dinos });
+});
+
+// New View
+// Most specific url
+app.get("/dinosaurs/new", (req, res) => {
+    res.render("dinosaurs/new")
+})
+
+// SHOW View
+app.get("/dinosaurs/:index", (req, res) => {
+    let dinos = fs.readFileSync("./dinosaurs.json");
+    // take out data and put it in a more readable format
+    dinos = JSON.parse(dinos);
+    // get the dino that's asked for
+    const dino = dinos[req.params.index];
+    res.render("dinosaurs/show", { dino });
+});
+
+
+// POST route, doesn't have a view
+
+app.post("/dinosaurs", (req, res) => {
+    // this is coming from out form submit
+    // we are going to look at the req.body
+    console.log(req.body)
+})
+
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${{PORT}}`);
+});
